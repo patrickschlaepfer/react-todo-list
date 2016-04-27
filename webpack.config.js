feb12f7884b69cdd2b99ce27b490a470388eff86
Patrick Schlaepfer <patrick@schlaepfer.com>
2016-04-27 23:24:36 +0200

    end of part 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
-
+module.exports = {
+	devtool: 'inline-source-map',
+	entry: ['./client/client.js'],
+	output: {
+		path: './dist', // where to put
+		filename: 'bundle.js', // file name
+		publicPath: '/'
+	},
+	module: {
+		loaders: [
+		{
+			test: /\.js$/, // pattern to next things
+			loader: 'babel-loader', // jsx... compiler
+			exclude: /node_modules/, // exclude
+			query: {
+				presets: ['react', 'es2015']
+			}
+		}
+		]
+	}
+}
